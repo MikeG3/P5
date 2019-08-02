4f5785b30c4ffe820428fa713b48e9e25e20138e
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-02 00:55:54 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -60,9 +60,11 @@ function setup() {
         soundWaves[i].setType('sine');
         soundWaves[i].amp(0.0);
         soundWaves[i].freq( (i*100) );
+        /*
         for ( j = 0 ; j < gridSizeY ; j++; ) {
             selectedSquares[gridSizeY][gridSizeX] = false;
         }//close for j
+        */
     }//close for
 }//close setup
 
