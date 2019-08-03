2dad34527eace2511a86869d2715aaabdb2e2b24
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-03 12:29:17 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -120,7 +120,7 @@ function draw() {
           
          //PLAY SOUND
          if ( selectedSquares[i][j] ){
-            soundWaves[i].amp(0.5);
+            soundWaves[i].amp();
          }//close if
          else {
               soundWaves[i].amp(0.0);
