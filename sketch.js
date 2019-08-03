f6da8efafafbbe8ae79ca96a2b219a1079dadf4c
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-03 00:26:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -114,9 +114,11 @@ function draw() {
          if ( selectedSquares[j][i] ){
             soundWaves[i].amp(0.5);
          }//close if
+          /*
          if ( !selectedSquares[j][i] ) {
               soundWaves[i].amp(0.0);
          }//close else not selected
+         /*
         }//close inner for loop
     }//close outter for loop
     
