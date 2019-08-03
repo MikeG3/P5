e074fc74c53a693b1edd47e0957cd5b9e97e1635
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-03 12:39:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -115,7 +115,7 @@ function draw() {
             fill(25, 25, 25);
         }//close if selected position square
         if ( j == xPos && i == yPos && selectedSquares[i][j]){
-            fill(255, 0, 200);
+            fill(75, 20, 50);
         }//close if selected position square
         //DRAW EACH RECTANGLE IN THE LOOPS
          rect(j*squareSize , i*squareSize , squareSize, squareSize); 
