5aa70d6f767112613c06a06a234fca60447b64f0
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-05 12:12:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -61,6 +61,7 @@ function setup() {
         soundWaves[i].setType('sine');
         soundWaves[i].amp(0.0);
         soundWaves[i].freq( (i*50+350) );
+        soundWaves[i].start();
         //INITIALIZE ALL SQUARES TO FLASE, NOT SELECTED
         selectedSquares[i] = new Array(); 
         for (j = 0 ; j < gridSizeX ; j++) {
