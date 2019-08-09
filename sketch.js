b4c465034e48701938be5f24cf6619358a216c51
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-08 23:53:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,7 +38,7 @@ var soundWaves = [gridSizeY];
 var selectedSquares = [gridSizeY];
 var hole, half, quarter, eigth;
 var tempo = 100;           //integer value used for counter to start/stop oscillator
-var noteDuration = 0.25;
+var noteDuration = 1;
 
 //RESPOND TO ARROW KEY INPUT (ASCII 37-40)
 window.addEventListener('keydown', move );
