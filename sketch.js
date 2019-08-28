fdcc6b065e18afd8dbb67a59b3d3bef480b6a100
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-28 12:50:01 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -80,7 +80,7 @@ function setup() {
       // Instantiate the envelope
       envelope = new p5.Env();
       // set attackTime, decayTime, sustainRatio, releaseTime
-      envelope.setADSR(0.001, 0.5, 0.1, 0.5);
+      envelope.setADSR(0.001, 0.25, 0.1, 0.25);
       // set attackLevel, releaseLevel
       envelope.setRange(1, 0);
       //
