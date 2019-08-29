0b2afdb634f6363ce5ec4a28e74e193bf6639bc2
Mike <39208141+MikeG3@users.noreply.github.com>
2019-08-29 15:49:51 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,7 +66,7 @@ function setup() {
     createCanvas(pWidth, pHeight);
     background( 95, 95, 95);
     //frameRate(60);
-     frameRate(4);
+     frameRate(20);
     //CONSTRUCT AND INTIALIZE SOUND WAVES FOR ALL ROWS
     for (i = 0 ; i < gridSizeY ; i++) {
         soundWaves[i] = new p5.Oscillator();
